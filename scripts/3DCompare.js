f6958049be58bbfcce49f87efd6a0bd7f449772e
zacfmhill <zacfmhill@gmail.com>
2024-06-20 13:00:33 -0400

    Initial 3D Compare Changes

---
+++ scripts/3DCompare.js
@@ -0,0 +1,121 @@
+import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { STLLoader } from 'three/addons/loaders/STLLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+const loader = new STLLoader();
+const progressBar = document.getElementById("fileUpProgress");
+const volumeModalText = document.getElementById("volumeDisplayText");
+var volumeModal = new bootstrap.Modal(document.getElementById('volumeCheckModal'));
+var volumeCalculated = 0; 
+var comparisonVolume = 0; 
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+var x = renderer.domElement;
+x.className = "ThreeJSCANVAS";
+document.body.appendChild(x);
+
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+// scene.fog = new THREE.Fog( 0xFF0000, 3, 6 );
+camera.position.z = 5;
+var controls = new OrbitControls(camera, renderer.domElement);
+
+document.getElementById("fileUP").onchange = fileUP;
+
+
+
+function animate() {
+    renderer.render(scene, camera);
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+}
+
+// Checking for WebGL support
+if (WebGL.isWebGLAvailable()) {
+    // Initiate function or other initializations here
+    renderer.setAnimationLoop(animate);
+} else {
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById('container').appendChild(warning);
+}
+
+
+function fileUP() {
+    var file = document.getElementById("fileUP").files[0];
+    if (file) {
+        progressBar.classList.add("bg-success");
+        progressBar.classList.remove("bg-danger");
+        loader.load(
+            URL.createObjectURL(file),
+            function (object) {            // called when resource is loaded
+                var mesh = new THREE.Mesh(object, new THREE.MeshBasicMaterial({
+                    color: 0xff00ff
+                  }));
+            mesh.rotation.set(-Math.PI / 2, 0, 0);
+            scene.add(mesh);
+            //Calculating Volume for validation:
+            volumeCalculated = getVolume(object);
+            console.log("stl volume is " + volumeCalculated);
+            volumeModalText.innerHTML = "STL volume is " + volumeCalculated;
+            volumeModal.show();
+            },
+            function (xhr) {            // called when loading is in progresses
+                progressBar.ariaValueNow = (xhr.loaded / xhr.total * 100);
+                progressBar.style = "width: " + (xhr.loaded / xhr.total * 100) + "%";
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+            },
+            function (error) { // called when loading has errors
+                progressBar.classList.remove("bg-success");
+                progressBar.classList.add("bg-danger");
+                console.log('An error happened');
+                console.log(error);
+            }
+        );
+    }
+}
+
+
+function getVolume(geometry) {
+    if (!geometry.isBufferGeometry) {
+      console.log("'geometry' must be an indexed or non-indexed buffer geometry");
+      return 0;
+    }
+    var isIndexed = geometry.index !== null;
+    let position = geometry.attributes.position;
+    let sum = 0;
+    let p1 = new THREE.Vector3(),
+      p2 = new THREE.Vector3(),
+      p3 = new THREE.Vector3();
+    if (!isIndexed) {
+      let faces = position.count / 3;
+      for (let i = 0; i < faces; i++) {
+        p1.fromBufferAttribute(position, i * 3 + 0);
+        p2.fromBufferAttribute(position, i * 3 + 1);
+        p3.fromBufferAttribute(position, i * 3 + 2);
+        sum += signedVolumeOfTriangle(p1, p2, p3);
+      }
+    }
+    else {
+      let index = geometry.index;
+      let faces = index.count / 3;
+      for (let i = 0; i < faces; i++){
+        p1.fromBufferAttribute(position, index.array[i * 3 + 0]);
+        p2.fromBufferAttribute(position, index.array[i * 3 + 1]);
+        p3.fromBufferAttribute(position, index.array[i * 3 + 2]);
+        sum += signedVolumeOfTriangle(p1, p2, p3);
+      }
+    }
+    return sum;
+  }
+  function signedVolumeOfTriangle(p1, p2, p3) {
+    return p1.dot(p2.cross(p3)) / 6.0;
+  }
