68cc6c0feaf689ab68e89d588c5712d5c215e35f
zacfmhill <zacfmhill@gmail.com>
2024-06-20 15:29:59 -0400

    Comparison Selection Now Working

--- scripts/3DCompare.js
+++ scripts/3DCompare.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
@@ -9,29 +10,34 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-const loader = new STLLoader();
+const stlLoader = new STLLoader();
+const objLoader = new OBJLoader();
 const progressBar = document.getElementById("fileUpProgress");
+const referenceBar = document.getElementById("fileSelectProgress")
 const volumeModalText = document.getElementById("volumeDisplayText");
-var volumeModal = new bootstrap.Modal(document.getElementById('volumeCheckModal'));
-var volumeCalculated = 0; 
-var comparisonVolume = 0; 
+const comparisonSelect = document.getElementById("comparisonFileSelect");
+const userImportMaterial = new THREE.MeshNormalMaterial();
+const referenceFileMaterial = new THREE.MeshBasicMaterial({ color: 0xFF0000, wireframe: true });
 
+var volumeModal = new bootstrap.Modal(document.getElementById('volumeCheckModal'));
+var volumeCalculated = 0;
+var comparisonVolume = 0;
 renderer.setSize(window.innerWidth, window.innerHeight);
 var x = renderer.domElement;
 x.className = "ThreeJSCANVAS";
 document.body.appendChild(x);
 
 // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// 
 // const cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
-// scene.fog = new THREE.Fog( 0xFF0000, 3, 6 );
+//scene.fog = new THREE.Fog( 0x3f7b9d, 0, 100 );
+const light = new THREE.AmbientLight(0x000000, 10); // soft white light
+scene.add(light);
 camera.position.z = 5;
 var controls = new OrbitControls(camera, renderer.domElement);
 
 document.getElementById("fileUP").onchange = fileUP;
-
-
 
 function animate() {
     renderer.render(scene, camera);
@@ -52,70 +58,131 @@ if (WebGL.isWebGLAvailable()) {
 function fileUP() {
     var file = document.getElementById("fileUP").files[0];
     if (file) {
-        progressBar.classList.add("bg-success");
-        progressBar.classList.remove("bg-danger");
-        loader.load(
-            URL.createObjectURL(file),
-            function (object) {            // called when resource is loaded
-                var mesh = new THREE.Mesh(object, new THREE.MeshBasicMaterial({
-                    color: 0xff00ff
-                  }));
-            mesh.rotation.set(-Math.PI / 2, 0, 0);
-            scene.add(mesh);
-            //Calculating Volume for validation:
-            volumeCalculated = getVolume(object);
-            console.log("stl volume is " + volumeCalculated);
-            volumeModalText.innerHTML = "STL volume is " + volumeCalculated;
-            volumeModal.show();
-            },
-            function (xhr) {            // called when loading is in progresses
-                progressBar.ariaValueNow = (xhr.loaded / xhr.total * 100);
-                progressBar.style = "width: " + (xhr.loaded / xhr.total * 100) + "%";
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-            },
-            function (error) { // called when loading has errors
+        if (!toString(comparisonSelect.value).includes("chall")) {
+            for( var i = scene.children.length - 1; i >= 0; i--) { 
+                let obj = scene.children[i];
+                scene.remove(obj); 
+            }           
+            progressBar.classList.add("bg-success");
+            progressBar.classList.remove("bg-danger");
+            referenceBar.classList.add("bg-success");
+            referenceBar.classList.remove("bg-danger");
+            if (file.name.endsWith("obj")) {
+                objLoader.load(
+                    URL.createObjectURL(file),
+                    function (object) {   // called when resource is loaded
+                        volumeCalculated = processOBJChildren(object, userImportMaterial);
+                        console.log("Upload volume is " + volumeCalculated);
+                    },
+                    loadingBar,
+                    loadingError
+                );
+            } else if (file.name.endsWith("stl")) {
+                stlLoader.load(
+                    URL.createObjectURL(file),
+                    function (object) {            // called when resource is loaded
+                        var mesh = new THREE.Mesh(object, userImportMaterial);
+                        // mesh.rotation.set(-Math.PI / 2, 0, 0);
+                        scene.add(mesh);
+                        //Calculating Volume for validation:
+                        volumeCalculated = getVolume(object);
+                        console.log("Upload volume is " + volumeCalculated);
+
+                    },
+                    loadingBar,
+                    loadingError
+                );
+            } else {
                 progressBar.classList.remove("bg-success");
                 progressBar.classList.add("bg-danger");
-                console.log('An error happened');
-                console.log(error);
+                progressBar.ariaValueNow = 100;
+                progressBar.style = "width: 100%";
+                alert("Error Reading file... Not STL or OBJ");
+                return;
             }
-        );
+            comparisonVolume = compareModels();
+        }
+        else {
+            alert("No Comparison Selected!");
+        }
     }
 }
 
+function loadingBar(xhr) {
+    progressBar.ariaValueNow = (xhr.loaded / xhr.total * 100);
+    progressBar.style = "width: " + (xhr.loaded / xhr.total * 100) + "%";
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+}
+
+function loadingError(error) {
+    progressBar.classList.remove("bg-success");
+    progressBar.classList.add("bg-danger");
+    console.log('An error happened');
+    console.log(error);
+}
 
 function getVolume(geometry) {
     if (!geometry.isBufferGeometry) {
-      console.log("'geometry' must be an indexed or non-indexed buffer geometry");
-      return 0;
+        console.log("'geometry' must be an indexed or non-indexed buffer geometry");
+        return 0;
     }
     var isIndexed = geometry.index !== null;
     let position = geometry.attributes.position;
     let sum = 0;
     let p1 = new THREE.Vector3(),
-      p2 = new THREE.Vector3(),
-      p3 = new THREE.Vector3();
+        p2 = new THREE.Vector3(),
+        p3 = new THREE.Vector3();
     if (!isIndexed) {
-      let faces = position.count / 3;
-      for (let i = 0; i < faces; i++) {
-        p1.fromBufferAttribute(position, i * 3 + 0);
-        p2.fromBufferAttribute(position, i * 3 + 1);
-        p3.fromBufferAttribute(position, i * 3 + 2);
-        sum += signedVolumeOfTriangle(p1, p2, p3);
-      }
+        let faces = position.count / 3;
+        for (let i = 0; i < faces; i++) {
+            p1.fromBufferAttribute(position, i * 3 + 0);
+            p2.fromBufferAttribute(position, i * 3 + 1);
+            p3.fromBufferAttribute(position, i * 3 + 2);
+            sum += signedVolumeOfTriangle(p1, p2, p3);
+        }
     }
     else {
-      let index = geometry.index;
-      let faces = index.count / 3;
-      for (let i = 0; i < faces; i++){
-        p1.fromBufferAttribute(position, index.array[i * 3 + 0]);
-        p2.fromBufferAttribute(position, index.array[i * 3 + 1]);
-        p3.fromBufferAttribute(position, index.array[i * 3 + 2]);
-        sum += signedVolumeOfTriangle(p1, p2, p3);
-      }
+        let index = geometry.index;
+        let faces = index.count / 3;
+        for (let i = 0; i < faces; i++) {
+            p1.fromBufferAttribute(position, index.array[i * 3 + 0]);
+            p2.fromBufferAttribute(position, index.array[i * 3 + 1]);
+            p3.fromBufferAttribute(position, index.array[i * 3 + 2]);
+            sum += signedVolumeOfTriangle(p1, p2, p3);
+        }
     }
     return sum;
-  }
-  function signedVolumeOfTriangle(p1, p2, p3) {
+}
+function signedVolumeOfTriangle(p1, p2, p3) {
     return p1.dot(p2.cross(p3)) / 6.0;
-  }
+}
+
+function processOBJChildren(object, material) {
+    var sumVolume = 0;
+    for (var i = 0; i < object.children.length; i++) {
+        let child = object.children[i];
+        scene.add(new THREE.Mesh(child.geometry, material));
+        sumVolume += getVolume(child.geometry);
+    }
+    return sumVolume;
+}
+
+function compareModels() {
+    var compVol = 0;
+    objLoader.load(
+        "/public/" + comparisonSelect.value,
+        function (object) {   // called when resource is loaded
+            compVol = processOBJChildren(object, referenceFileMaterial);
+            console.log("Reference volume is " + compVol);
+            volumeModalText.innerHTML = "Upload volume is " + volumeCalculated + "\nReference volume is " + compVol;
+            volumeModal.show();
+        },
+        function(xhr){
+            referenceBar.ariaValueNow = (xhr.loaded / xhr.total * 100);
+            referenceBar.style = "width: " + (xhr.loaded / xhr.total * 100) + "%";
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        loadingError
+    );
+    return compVol;
+}
